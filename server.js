94357f2ee3ca8143b4075cb9465ba36f6a36afc3
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-08-21 10:20:34 +0530

    swagger

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+
+//swagger
+const  sweggerjsdoc =require('swagger-jsdoc');
+const swaggerui =require('swagger-ui-express');
+const  swaggerdocument = require('../swagger.json')
+// /
 const { connection } = require("./db");
 require("dotenv").config();
 const cors = require("cors");
@@ -17,10 +23,23 @@ const emailtwoRouter = require("./modules/Email/email.routes");
 
 const app = express(); 
 
+
+
+
+
+
+
+
 // Middleware
 app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.static("uploads"));
+
+
+// swagger ui setup
+
+app.use('/api-doc',swaggerui.serve,swaggerui.setup(swaggerdocument));
+
 
 // Routes
 app.use('/', Homerouter);
