8bd78fec10b388b6676672318bf7ad337ce8180a
satyen mori <satyenmori@gmail.com>
2024-08-23 16:47:40 +0530

    editor changes and set all controller tost message

--- modules/Email/email.routes.js
+++ modules/Email/email.routes.js
@@ -3,6 +3,10 @@ const emailtwoRouter = express.Router();
 
 const emailcatalogController = require("../Email/emailController");
 
-emailtwoRouter.post("/twoemail/send-email", emailcatalogController.createContactmail);
-
+emailtwoRouter.post(
+  "/twoemail/send-email",
+  emailcatalogController.createContactmail
+);
+emailtwoRouter.get("/email", emailcatalogController.getEmail);
+emailtwoRouter.delete("/email/delete/:id", emailcatalogController.deleteEmail);
 module.exports = emailtwoRouter;
--- modules/deskfounder/deskfounderController.js
+++ modules/deskfounder/deskfounderController.js
@@ -1,43 +1,64 @@
-const {deskfounderModel } = require('./deskfounderModel');
-
+const { deskfounderModel } = require("./deskfounderModel");
+const mongoose = require('mongoose');
 // Create a new post
 
 exports.addDeskfounder = async (req, res) => {
   try {
-    const {author_name,description,filename } = req.body;
+    const { author_name, description, filename } = req.body;
     // const deskfounderFile = req.file.filename;
     const deskfounderFile = req.file ? req.file.filename : null;
 
-
     const Deskfounder = new deskfounderModel({
       author_name,
-      description, 
-        Deskfounder_images: deskfounderFile,
-    }); 
+      description,
+      Deskfounder_images: deskfounderFile,
+    });
     await Deskfounder.save();
-    res.status(200).json({ msg: "deskfounderF add successfuly", Deskfounder });
+    res.status(200).json({ msg: "add successfuly", data:Deskfounder });
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: "Couldn't Add deskfounder", error });
   }
 };
 
-
 // Get all deskfounder or a single deskfounder by ID
 
+// exports.getDeskfounder = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     if (id) {
+//       const Deskfounder = await deskfounderModel.findById(id);
+//       if (!Deskfounder) {
+//         return res.status(404).send({ msg: "deskfounder not found" });
+//       }
+
+//       res.status(200).send(Deskfounder);
+//     } else {
+//       const posts = await deskfounderModel.find();
+
+//       res.status(200).send(posts);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res
+//       .status(400)
+//       .send({ msg: "Couldn't Retrieve deskfounder", details: error });
+//   }
+// };
 exports.getDeskfounder = async (req, res) => {
   try {
     const { id } = req.params;
     if (id) {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ msg: "Invalid ID format" });
+      }
       const Deskfounder = await deskfounderModel.findById(id);
       if (!Deskfounder) {
-        return res.status(404).send({ msg: "deskfounder not found" });
+        return res.status(404).send({ msg: "Deskfounder not found" });
       }
-      
       res.status(200).send(Deskfounder);
     } else {
       const posts = await deskfounderModel.find();
-      
       res.status(200).send(posts);
     }
   } catch (error) {
@@ -45,7 +66,6 @@ exports.getDeskfounder = async (req, res) => {
     res.status(400).send({ msg: "Couldn't Retrieve deskfounder", details: error });
   }
 };
-
 
 // Update a post by ID
 exports.updateDeskfounder = async (req, res) => {
@@ -56,15 +76,19 @@ exports.updateDeskfounder = async (req, res) => {
   try {
     const Deskfounder = await deskfounderModel.findById(id);
     if (!Deskfounder) {
-      return res.status(404).send({ msg: 'Deskfounder not found' });
+      return res.status(404).send({ msg: "Deskfounder not found" });
     }
 
     const updateData = {
       ...updates,
       Deskfounder_images: deskfounderFile || Deskfounder.Deskfounder_images,
     };
-    const updateDeskfounder = await deskfounderModel.findByIdAndUpdate(id, updateData, { new: true });
-    res.status(200).send(updateDeskfounder);
+    const updateDeskfounder = await deskfounderModel.findByIdAndUpdate(
+      id,
+      updateData,
+      { new: true }
+    );
+    res.status(200).send({msg:"Update Successfuly",data:updateDeskfounder});
   } catch (error) {
     console.error(error);
     res.status(400).send({ msg: "Couldn't Update Deskfounder", error });
@@ -72,15 +96,15 @@ exports.updateDeskfounder = async (req, res) => {
 };
 
 // Delete a post by ID
-exports.deleteDeskfounder = async (req, res) => {  
+exports.deleteDeskfounder = async (req, res) => {
   const { id } = req.params;
 
   try {
     const deletedPost = await deskfounderModel.findByIdAndDelete(id);
     if (!deletedPost) {
-      return res.status(404).send({ msg: 'Post not found' });
+      return res.status(404).send({ msg: "Post not found" });
     }
-    res.status(200).send({ msg: 'Post deleted successfully' });
+    res.status(200).send({ msg: "Post deleted successfully" });
   } catch (error) {
     res.status(400).send({
       msg: "Couldn't Delete deskfounder",
